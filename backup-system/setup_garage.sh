5a805f2683e6a0ba2ac25369aa5504cd26f2008b
deanqx <dean2@kowatsch.de>
2026-06-14 18:55:01 +0200

    feat: add example backup script, improve Garage setup script

--- backup-system/setup_garage.sh
+++ backup-system/setup_garage.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 # ref: https://garagehq.deuxfleurs.fr/documentation/cookbook/real-world/
 
-docker compose exec s3_object_storage /garage key import \
+# Exit on error, uninitialized variable or pipefail
+set -euo pipefail
+
+shopt -s expand_aliases
+alias garage="docker compose exec s3_object_storage /garage"
+
+garage key import \
 $GARAGE_DEFAULT_ACCESS_KEY $GARAGE_DEFAULT_SECRET_KEY --yes
 
-docker compose exec s3_object_storage /garage key allow \
-$GARAGE_DEFAULT_ACCESS_KEY --create-bucket
+garage bucket create home-assistant
 
 # docker compose exec s3_object_storage /garage node id
 # 64cc87709556109bd326653a60ae4a3108df1f8952c9b5592004ce0bfba1d6bb@[::1]:3901
 # ...
-node_id=$(sudo docker compose exec s3_object_storage /garage node id |& head -n 1 | cut -d'@' -f1)
+node_id=$(garage node id |& head -n 1 | cut -d'@' -f1)
 
-sudo docker compose exec s3_object_storage /garage layout assign $node_id -c 10GB -z zone1
+garage layout assign $node_id -c 10GB -z zone1
 
-sudo docker compose exec s3_object_storage /garage layout show
+garage layout show
 
 echo
 echo "After you have verified the layout, apply it like this:"
